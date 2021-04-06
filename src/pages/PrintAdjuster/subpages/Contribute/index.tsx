fc054fad28e081603d43125a41c0c0501a40e7fd
Michel Kremer <kremi151dev@gmail.com>
2021-04-06 22:19:15 +0200

    Fix typo

--- src/pages/PrintAdjuster/subpages/Contribute/index.tsx
+++ src/pages/PrintAdjuster/subpages/Contribute/index.tsx
@@ -7,8 +7,8 @@ export default function ContributePage() {
             <Typography variant="h5">
                 Become a Beta tester
             </Typography>
-            <Typography paragraph style={{whiteSpace: 'pre-line'}}>
-                You can subscribe to become a Beta tester of PrintAdjuster.<br/>
+            <Typography paragraph>
+                You can subscribe to become a Beta tester of Print adjuster.<br/>
                 As a Beta tester, you will receive new updates earlier and you can help evaluating them before the official release.<br/>
                 You can subscribe <a href={"https://play.google.com/apps/testing/lu.kremi151.printresizer"}>here</a>, and any feedback is welcome via <a href={"mailto:kremi151dev@gmail.com"}>email</a>.
             </Typography>
@@ -16,7 +16,7 @@ export default function ContributePage() {
             <Typography variant="h5">
                 Got ideas for improvements or new features?
             </Typography>
-            <Typography paragraph style={{whiteSpace: 'pre-line'}}>
+            <Typography paragraph>
                 Send me an <a href={"mailto:kremi151dev@gmail.com"}>email</a>.
             </Typography>
         </div>
