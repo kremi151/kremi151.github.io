86910e756477b930374e3a41ab815eb684bdc484
Michel Kremer <kremi151dev@gmail.com>
2021-04-09 18:53:17 +0200

    Add carousel with Print adjuster screenshots

--- src/pages/PrintAdjuster/subpages/Start/index.tsx
+++ src/pages/PrintAdjuster/subpages/Start/index.tsx
@@ -1,12 +1,90 @@
-import React from 'react';
-import {Typography} from "@material-ui/core";
+import React, {useMemo} from 'react';
+import {makeStyles, Typography} from "@material-ui/core";
 import PlayStoreBadge from "../../../../components/PlayStoreBadge";
+import Carousel from "react-material-ui-carousel";
+
+const useStyles = makeStyles((theme) => ({
+    carouselItemRoot: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    carouselCaption: {
+        marginTop: 8,
+    },
+    carouselNavButton: {
+        backgroundColor: `${theme.palette.secondary.main} !important`,
+    },
+    carouselActiveIndicator: {
+        color: theme.palette.secondary.main,
+    },
+}));
+
+interface ScreenshotProps {
+    readonly src: string;
+    readonly description: string;
+}
+
+const screenshots: ScreenshotProps[] = [
+    {
+        src: '/screenshots/print_adjuster/Screenshot_1617986827.png',
+        description: 'Move and resize pictures on a page',
+    },
+    {
+        src: '/screenshots/print_adjuster/Screenshot_1617984502.png',
+        description: 'Multiple pictures are supported as well',
+    },
+    {
+        src: '/screenshots/print_adjuster/Screenshot_1617984342.png',
+        description: 'Chose from a variety of options',
+    },
+    {
+        src: '/screenshots/print_adjuster/Screenshot_1617984353.png',
+        description: 'Crop pictures from within the app',
+    },
+    {
+        src: '/screenshots/print_adjuster/Screenshot_1617984377.png',
+        description: 'Apply predefined filters to your pictures',
+    },
+    {
+        src: '/screenshots/print_adjuster/Screenshot_1617986748.png',
+        description: 'Add texts to your page',
+    },
+    {
+        src: '/screenshots/print_adjuster/Screenshot_1617986979.png',
+        description: 'Create multiple pages',
+    },
+    {
+        src: '/screenshots/print_adjuster/Screenshot_1617987065.png',
+        description: 'Print your work',
+    },
+];
+
+function Screenshot(props: ScreenshotProps) {
+    const styles = useStyles();
+    const { src, description } = props;
+    return (
+        <div className={styles.carouselItemRoot}>
+            <img src={src} width={250} alt={description}/>
+            <Typography className={styles.carouselCaption}>
+                {description}
+            </Typography>
+        </div>
+    );
+}
 
 export default function StartPage() {
+    const styles = useStyles();
+    const navButtonProps = useMemo(() => ({ className: styles.carouselNavButton }), [styles]);
+    const activeIndicatorIconButtonProps = useMemo(() => ({ className: styles.carouselActiveIndicator }), [styles]);
     return (
         <div>
-            <Typography paragraph>
-                This page is currently under construction 🚧
+            <Carousel navButtonsProps={navButtonProps} activeIndicatorIconButtonProps={activeIndicatorIconButtonProps}>
+                {screenshots.map((p) => <Screenshot {...p} />)}
+            </Carousel>
+
+            <Typography variant="h5">
+                Get the app
             </Typography>
             <PlayStoreBadge appPackageName="lu.kremi151.printresizer" />
         </div>
