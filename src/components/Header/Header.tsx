04f4baed96199d5c4930f7e9ebf642d3f5849ecf
Michel Kremer <kremi151dev@gmail.com>
2021-08-03 22:25:37 +0200

    Responsive layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,23 @@
-import React, {useCallback} from "react";
-import SearchIcon from '@material-ui/icons/Search';
+import React, {useCallback, useEffect, useMemo, useState} from "react";
 import GithubIcon from '@material-ui/icons/GitHub';
+import MenuIcon from '@material-ui/icons/Menu';
 import Link from "@material-ui/core/Link";
-import {Link as RouterLink} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 import {IconButton} from "@material-ui/core";
 import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import useMediaQuery from "@material-ui/core/useMediaQuery/useMediaQuery";
+import useTheme from "@material-ui/core/styles/useTheme";
+import Drawer from "@material-ui/core/Drawer/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        borderBottom: '1px solid #dedede',
+    },
     toolbar: {
         borderBottom: `1px solid ${theme.palette.divider}`,
     },
@@ -21,6 +29,7 @@ const useStyles = makeStyles((theme) => ({
         overflowX: 'auto',
     },
     toolbarLink: {
+        cursor: 'pointer',
         padding: theme.spacing(1),
         flexShrink: 0,
     },
@@ -36,13 +45,43 @@ interface Props {
     sections: HeaderSection[];
 }
 
-export function Header({ title, sections }: Props) {
+export function Header({ title, sections: inSections }: Props) {
+    const { breakpoints } = useTheme();
     const classes = useStyles();
 
+    const history = useHistory();
+
+    const small = useMediaQuery(breakpoints.down('xs'));
+
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const openDrawer = useCallback(() => {
+        setDrawerOpen(true);
+    }, [setDrawerOpen]);
+    const closeDrawer = useCallback(() => {
+        setDrawerOpen(false);
+    }, [setDrawerOpen]);
+    useEffect(() => {
+        if (!small) {
+            closeDrawer();
+        }
+    }, [small, closeDrawer]);
+
+    const sections = useMemo(() => inSections.map(s => ({
+        title: s.title,
+        onClick: () => {
+            setDrawerOpen(false);
+            history.push(s.route);
+        },
+    })), [inSections, history, setDrawerOpen]);
+
     return (
-        <>
+        <div className={classes.root}>
             <Toolbar>
-                <Button size="small">Subscribe</Button>
+                {small && (
+                    <IconButton onClick={openDrawer}>
+                        <MenuIcon />
+                    </IconButton>
+                )}
                 <Typography
                     component="h2"
                     variant="h5"
@@ -53,29 +92,40 @@ export function Header({ title, sections }: Props) {
                 >
                     {title}
                 </Typography>
-                <IconButton>
-                    <SearchIcon />
-                </IconButton>
                 <IconButton href="https://github.com/kremi151">
                     <GithubIcon />
                 </IconButton>
             </Toolbar>
-            <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
-                {sections.map((section) => (
-                    <Link
-                        component={RouterLink}
-                        color="inherit"
-                        noWrap
-                        key={section.title}
-                        variant="body2"
-                        href={section.route}
-                        className={classes.toolbarLink}
-                        to={section.route}
-                    >
-                        {section.title}
-                    </Link>
-                ))}
-            </Toolbar>
-        </>
+            {small ? (
+                <Drawer anchor="left" open={drawerOpen} onClose={closeDrawer}>
+                    <List>
+                        {sections.map(({ title, onClick }) => (
+                            <ListItem
+                                key={title}
+                                button
+                                onClick={onClick}
+                            >
+                                <ListItemText primary={title} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Drawer>
+            ) : (
+                <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
+                    {sections.map(({ title, onClick }) => (
+                        <Link
+                            color="inherit"
+                            noWrap
+                            key={title}
+                            variant="body2"
+                            className={classes.toolbarLink}
+                            onClick={onClick}
+                        >
+                            {title}
+                        </Link>
+                    ))}
+                </Toolbar>
+            )}
+        </div>
     );
 }
